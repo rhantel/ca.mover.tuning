2fb3e345557cb421040200b65ea3916d58ffdadc
Castcore <88655407+Castcore@users.noreply.github.com>
2023-06-19 03:40:23 +1000

    Changes made to stop issues with whitespaces for the file types, file lists, before script, or after script variables.

--- source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/mover.php
+++ source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/mover.php
@@ -49,8 +49,9 @@ function startMover($options="") {
 
 	if ($cron or $cfg['movenow'] == "yes") {
 		//exec("echo 'running from cron or move now question is yes' >> /var/log/syslog");
-		$beforescript = $cfg['beforeScript'];
-                $afterscript = $cfg['afterScript'];
+		$delimitter = "?|+?"; #To replace spaces, chosen so it is very unlikely to be an issue
+		$beforescript = str_replace(' ', $delimitter, trim($cfg['beforeScript'])); 	#Remove/replace whitsespace incase path to script has them
+        $afterscript = str_replace(' ', $delimitter, trim($cfg['afterScript']));
 
 		if ($cfg['threshold'] >= 0 or $cfg['age'] == "yes" or $cfg['sizef'] == "yes" or $cfg['sparsnessf'] == "yes"  or $cfg['filelistf'] == "yes" or $cfg['filetypesf'] == "yes" or $beforescript != '' or $afterscript != '' or $cfg['testmode'] == "yes" ) {
 
@@ -60,8 +61,8 @@ function startMover($options="") {
 			$ageLevel = $cfg['daysold'];
 			$sizeLevel = $cfg['sizeinM'];
 			$sparsnessLevel = $cfg['sparsnessv'];
-			$filelistLevel = $cfg['filelistv'];
-			$filetypesLevel = $cfg['filetypesv'];
+			$filelistLevel = str_replace(' ', $delimitter, trim($cfg['filelistv']));
+			$filetypesLevel = str_replace(' ', '', trim($cfg['filetypesv']));
 			$ctime = $cfg['ctime'];
 			$testmode = $cfg['testmode'];
 			$omoverth = $cfg['omoverthresh'];
@@ -112,29 +113,29 @@ function startMover($options="") {
 			}
 			if (empty($ctime)) {
 				$age_mover_str = "$age_mover_str ''";
-			}
+			} 
 			else {
 				$age_mover_str = "$age_mover_str $ctime";
 			}
-                        if ($cfg['omovercfg'] == "yes") {
-                                $age_mover_str = "$age_mover_str $omoverth";
-                        }
-                        else {
-                                $age_mover_str = "$age_mover_str ''";
-                        }
-
-                        if ($cfg['testmode'] == "yes") {
-				$age_mover_str = "$age_mover_str 'yes'";
-                        }
-                        else {
+			if ($cfg['omovercfg'] == "yes") {
+				$age_mover_str = "$age_mover_str $omoverth";
+			} 
+			else {
 				$age_mover_str = "$age_mover_str ''";
-                        }
+			}
+			if ($cfg['testmode'] == "yes") {
+				$age_mover_str = "$age_mover_str 'yes'";
+			} 
+			else {
+				$age_mover_str = "$age_mover_str ''";
+			}
 			if ($cfg['ignoreHidden'] == "yes") {
 				$age_mover_str = "$age_mover_str 'yes'";
 			}
 			else {
 				$age_mover_str = "$age_mover_str ''";
 			}
+			$age_mover_str = "$age_mover_str $delimitter"; 	#Add delimitter for age_mover to use
 
 			//exec("echo 'about to hit mover string here: $age_mover_str' >> /var/log/syslog");
 
